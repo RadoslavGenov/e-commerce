44d37298abeefb27dacf531a8927c1eec3930bb6
Radoslav Genov <radoslavgenov5@hotmail.com>
2024-01-28 19:42:54 +0200

    a little more configuration for static files

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,18 @@ import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 import { CartModule } from './modules/cart/cart.module';
 import { ProductModule } from './modules/product/product.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [DatabaseModule, AuthModule, UserModule, CartModule, ProductModule],
+  imports: [
+    DatabaseModule,
+    AuthModule,
+    UserModule,
+    CartModule,
+    ProductModule,
+    ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,32 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import {
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Ecommerce Store')
+    .setDescription('The ECommerce API description')
+    .setVersion('1.0')
+    .addTag('store')
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+
+  const document = SwaggerModule.createDocument(app, config, options);
+
+  SwaggerModule.setup('api', app, document);
 
   app.enableCors();
 
